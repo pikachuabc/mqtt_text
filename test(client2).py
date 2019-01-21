3c1a672add558d1dab0465b011b6348eeac32379
jiafan <596137647@qq.com>
2019-01-21 18:30:19 +0800

    发送图片测试

--- test(client2).py
+++ test(client2).py
@@ -1,8 +1,9 @@
 import paho.mqtt.client as mqtt
-
+import base64
+import os
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc)+",连接成功")
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -10,8 +11,15 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + ":" + msg.payload.decode())
+    global count
+    print(msg.payload)
+    img_data = base64.b64decode(msg.payload)
+    file = open('{}.jpg'.format(count), 'wb')
+    count = count + 1
+    file.write(img_data)
+    file.close()
 
+count = 3
 client = mqtt.Client()
 client.on_connect = on_connect
 """回调函数，这里的client.on_connect是作为属性的，需要传入函数的地址"""
@@ -19,9 +27,4 @@ client.on_message = on_message
 HOST = '127.0.0.1'
 #HOST = '192.168.1.104'
 client.connect(HOST, 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
--- test(publisher).py
+++ test(publisher).py
@@ -1,29 +1,27 @@
 import paho.mqtt.client as mqtt
 import sqlite3
 import base64
+import time
 
 pub_topic = "chat"
 HOST = '127.0.0.1'
 #HOST = '192.168.1.104'
 
 
-def get_sensor_data():
-    pass
-
-
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc)+",连接成功")
     client.subscribe("chat")
 
 
 def send_img():
-    path = input("请输入发送文件路径及名称")
+    path= input("请输入发送照片路径")
     base64_data = base64.b64encode(open(path,'rb').read())
-    return base64_data
+    print(base64_data)
+    client.publish(pub_topic,base64_data,0)
 
 
 if __name__ == '__main__':
-    client = mqtt.Client()
+    client = mqtt.Client("client1", userdata="yonghushuju")
     try:
         sensor_No = "21"
         client.will_set(pub_topic, "sensor " + sensor_No + " is offline")
@@ -32,7 +30,8 @@ if __name__ == '__main__':
         print('连接失败')
     else:
         client.on_connect = on_connect
-        client.loop_forever()
+        client.loop_start()
+        time.sleep(1)
     while True:
         send_img()
 #嘿嘿嘿
